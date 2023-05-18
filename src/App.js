7707320efca9c809cb438b4e4056987586ae1c85
Diogo <d8barcelos@gmail.com>
2023-05-18 15:32:02 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const App = () => {
   };
 
   return (
-    <div className="bg-gray-800 h-screen grid place-items-center container">
+    <div className="bg-gray-800 h-screen grid place-items-center">
       <div className="bg-white w-96 p-4 rounded-md">
         <div className="flex items-center justify-between">
           <input
